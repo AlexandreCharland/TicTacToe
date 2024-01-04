48e92fd2b24a298cf514726e92e7070b59bb1cf9
Alexandre Charland <Alexandre@Charland.com>
2024-01-04 15:57:13 -0500

    Change JAN notation and updated ShowPosition

--- GameManager.jl
+++ GameManager.jl
@@ -1,28 +1,35 @@
 # Numerical notation equivalence in alphabetical notation 0 = S, 1 = s, 2 = M, 3 = m, 4 = L, 5 = l
+translation = Dict("" => " ", '0' => "S", '1' => "s", '2' => "M", '3' => "m", '4' => "L", '5' => "l")
 
-#JAN #a b c d e f g h i /
+# JAN  a b c d e f g h i#/
 # # = 0 if it is player 0's move and # = 1 if it is player 1's move
 #  a | b | c
 # ---+---+---
 #  d | e | f
 # ---+---+---
 #  g | h | i
-# Whatever is in the spot a gets recorded in numerical notation. Nothing mean there isn't anything in the spot.
+# Whatever is in the spot a gets recorded behind a in numerical notation.
+# Nothing mean there isn't anything in the box.
 # After the / is the not yet played piece in order of numerical notation
-# This is the JAN of the starting position : "0abcdefghi/001122334455"
+# This is the JAN of the starting position : "abcdefghi0/001122334455"
+
+# A move will be denoted as the following #ab
+# # is the piece in play using the numerical notation
+# a is where that piece come from. If the piece come from outside of the board use " ".
+# If the piece is already place and we want to move it, then use the current position of the piece.
+# b is the box where the piece ends up.
 
 #This function take a JAN and return the state of the board of the JAN in alphabetical notation.
 function ShowPosition(JAN)
-    translation = Dict("" => " ", '0' => "S", '1' => "s", '2' => "M", '3' => "m", '4' => "L", '5' => "l")
-    letter = 98
+    letter = 97
     line = " "
     stuff = ""
-    for i in 3:(findfirst('/', JAN)-1)
-        if(Char(letter) != JAN[i])
+    for i in 1:(findfirst('i', JAN)-1)
+        if (Char(letter) != JAN[i])
             stuff = JAN[i]
         else
             line = string(line, translation[stuff])
-            if(letter % 3 == 1) #only activate for d and g
+            if (letter % 3 == 0) #only activate for c and f
                 println(line)
                 println("---+---+---")
                 line = " "
@@ -37,9 +44,8 @@ function ShowPosition(JAN)
     println(line)
 end
 
-#This function take a JAN and return 0 if player 0 has won and 1 if player 1 has won.
-#It return 2 if the game isn't done
-function WinnerDecided(JAN)
+#This function take a JAN and return false if no player has won and true if a player won.
+function SomeoneWon(JAN)
     
 end
 
@@ -48,9 +54,9 @@ function VerifyMove(JAN, move)
     
 end
 
-#This fonction take a JAN and return the original JAN if the move is illegal.
-#If the move is legal it will return the JAN of the new position.
+#This fonction take a JAN and return the JAN of the new position.
+#It assume that the move is always possible
 function MakeMove(JAN, move)
-    
+
 end
-#ShowPosition("0abcdefghi/001122334455")
+ShowPosition("abcdefghi0/001122334455")
